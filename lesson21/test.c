12520a71f99dc57f864f4635704361137b2fda2f
byXhy <xhydyxiang@sina.com>
2015-09-04 19:15:38 +0800

    add lesson21/test.c

---
+++ lesson21/test.c
@@ -0,0 +1,45 @@
+/*
+ ********************************************************************************
+ *      Copyright (C), 2015-2115, Xhy Tech. Stu.
+ *
+ *      FileName   : test.c
+ *
+ *      Author     : X h y
+ *
+ *      Version    : 1.0
+ *
+ *      Date       : 09-04-2015
+ *
+ *      Description: 
+ ********************************************************************************
+ */
+ 
+/**
+ * \file
+ * \brief  signed vs unsigned     1000   vs   0111  (88   vs   78)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned int a1 = 0x000000088;
+
+    char* b1= (char*)&a1;
+
+    printf("*b1 = %08x\n", *b1);
+    printf("(unsigned char)a1 = %08x\n", (unsigned char)a1);
+    printf("------------------------------------------------------\n");
+
+    printf("\n");
+    unsigned int a2 = 0xF0000078;
+
+    char* b2 =(char*)&a2;
+    
+    printf("*b2 = %08x\n", *b2);
+    printf("(unsigned char)a2 = %08x\n", (unsigned char)a2);
+
+    return 0;
+}
+
+/* end of file */
